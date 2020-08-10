5ce1c0b7468a94c3ff96d9fb3336270aafe037f2
Bastian Eicher <bastian@eicher.net>
2020-08-10 10:25:40 +0200

    Report tests results

--- src/test.sh
+++ src/test.sh
@@ -2,4 +2,4 @@
 set -e
 cd `dirname $0`
 
-dotnet test --no-build --configuration Release UnitTests/UnitTests.csproj
+dotnet test --no-build --logger junit --configuration Release UnitTests/UnitTests.csproj
